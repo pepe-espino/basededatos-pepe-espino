acd0659f47d7e81488574cf09c0889c544c40ed8
Jose Antonio Espino <joseantespinodz@gmail.com>
2025-04-08 08:22:39 +0200

    boletin2PLSQL

--- PLSQL/boletin2PLSQL.sql
+++ PLSQL/boletin2PLSQL.sql
@@ -5,13 +5,26 @@
 --mínimo, máximo y medio del departamento de FINANZAS. Debe hacerse
 --uso de cursores implícitos, es decir utilizar SELECT ... INTO.
 
-DECLARE
-	E_CURSOR EMPLEADOS%ROWTYPE;
+CREATE OR REPLACE PROCEDURE finanzas IS
+    v_num_empleados NUMBER;
+    v_sal_min NUMBER;
+    v_sal_max NUMBER;
+    v_sal_avg NUMBER;
 BEGIN
-	SELECT COUNT(E.NUMEM), MIN(E.SALAR), MAX(E.SALAR), AVG(E.SALAR) FROM EMPLEADOS e
-	JOIN DEPARTAMENTOS d ON D.NUMDE = E.NUMDE
-	WHERE UPPER(D.NOMDE) LIKE 'FINANZAS';	
-	
-	
-	
-END
+    SELECT COUNT(*), MIN(SALAR), MAX(SALAR), AVG(SALAR)
+    INTO v_num_empleados, v_sal_min, v_sal_max, v_sal_avg
+    FROM EMPLEADOS e, departamentos d
+    WHERE e.NUMDE = d.numde;
+				
+
+    DBMS_OUTPUT.PUT_LINE('Num empleados: ' || v_num_empleados);
+    DBMS_OUTPUT.PUT_LINE('Salario min: ' || v_sal_min);
+    DBMS_OUTPUT.PUT_LINE('Salario max: ' || v_sal_max);
+   	DBMS_OUTPUT.PUT_LINE('Salario med: ' || ROUND(v_sal_avg, 2));
+END;
+
+BEGIN finanzas;
+END;
+
+----
+
